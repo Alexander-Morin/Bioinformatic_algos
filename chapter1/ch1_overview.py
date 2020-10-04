411719bfa6fb3486af2099dab56dca8e45607e67
Alexander-Morin <alexm.14@outlook.com>
2020-10-04 16:39:43 -0700

    testing format of overview

--- chapter1/ch1_overview.py
+++ chapter1/ch1_overview.py
@@ -1,1 +1,10 @@
 # Chapter 1 is centered around text matching algorithms, framed by asking how to find the ori in bacteria.
+
+def pattern_count(text, pattern):
+    """Assumes text and pattern are strings. Returns the count of times a pattern was found in a string"""
+    count = 0
+    k = len(pattern)
+    for i in range(0, len(text) - k+1):
+        if text[i:i+k] == pattern:
+            count =+ 1
+    return count
