911eac4a491682662ab6c7112afa872716b93bd5
Alexander-Morin <alexm.14@outlook.com>
2021-06-08 17:31:17 -0700

    init chapter 4 and protein translation problem

---
+++ ch4_overview.py
@@ -0,0 +1,31 @@
+# This chapter explores translation/encoding proteins, motivated by antibiotics discovery
+
+import pandas as pd
+
+
+# Text begins by motivating how Bacillus brevis creates/encodes the antibiotic Tyrocidine B1, which is 10 aa long
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def get_genetic_code(bases="UCAG", acids="FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"):
+    # https://www.petercollingridge.co.uk/tutorials/bioinformatics/codon-table/
+    codons = [a + b + c for a in bases for b in bases for c in bases]
+    codon_table = dict(zip(codons, acids))
+    return codon_table
+
+
+def protein_translation(pattern, genetic_code):
+    protein = []
+    for i in range(len(pattern) - 3):
+        codon = pattern[i:i+3]
+        acid = genetic_code[codon]
+        if acid is "*":
+            break
+        protein.append(acid)
+    return protein
+
+
+
+genetic_code = get_genetic_code()
+input_text = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+print(protein_translation(input_text, genetic_code))
