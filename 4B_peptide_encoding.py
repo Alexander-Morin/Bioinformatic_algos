a3caede27caf5cb4ffd691d15a79abd53282c35c
Alexander-Morin <alexm.14@outlook.com>
2021-06-10 16:54:37 -0700

    4B passed rosalind grader - still need to doc

--- 4B_peptide_encoding.py
+++ 4B_peptide_encoding.py
@@ -1,13 +1,22 @@
-# This chapter explores translation/encoding proteins, motivated by antibiotics discovery
+# Implementing
 
-import pandas as pd
+# Problem 4B in the BALA textbook/Rosalind
 
+# Input
 
-# Text begins by motivating how Bacillus brevis creates/encodes the antibiotic Tyrocidine B1, which is 10 aa long
+# Output
+
+# Usage: python3 4B_peptide_encoding.py input.txt > output.txt
 # ----------------------------------------------------------------------------------------------------------------------
+
+import sys
+import fileinput
 
 
 def get_genetic_code(string_type="DNA"):
+    """
+    returns a dictionary mapping the 64 codons to the 20 amino acids + stop codons
+    """
     # https://www.petercollingridge.co.uk/tutorials/bioinformatics/codon-table/
     if string_type is "RNA":
         bases = "UCAG"
@@ -20,6 +29,11 @@ def get_genetic_code(string_type="DNA"):
 
 
 def protein_translation(pattern, genetic_code):
+    """
+    pattern: an RNA string
+    genetic_code: a dict mapping RNA codons to amino acids/stop codons
+    returns the translated protein string
+    """
     protein = []
     for i in range(0, len(pattern), 3):  # step of 3 to represent codon
         codon = pattern[i:i+3]
@@ -28,11 +42,6 @@ def protein_translation(pattern, genetic_code):
             break
         protein.append(acid)
     return "".join(protein)
-
-
-# Next, look at finding which DNA strings encode for a given peptide string. Need to consider all three open reading
-# frames as well as both strands
-# ----------------------------------------------------------------------------------------------------------------------
 
 
 def get_reverse_complement(dna_string):
@@ -68,16 +77,27 @@ def peptide_encoding(dna_string, peptide_string):
     return substrings
 
 
+def main():
+    """
+    Read the input file, parse the arguments,
+    """
+    argv = list(sys.argv)
+    input_dna_string = None
+    input_peptide_string = None
+
+    for line in fileinput.input(argv[1]):
+        if len(line) > 0:
+            if input_dna_string is None:
+                input_dna_string = str(line.replace('\n', ''))
+            else:
+                input_peptide_string = str(line.replace('\n', ''))
 
 
+    code = get_genetic_code(string_type="RNA")
+    output = peptide_encoding(input_dna_string, input_peptide_string)
+    for sequence in output:
+        print(sequence)
 
 
-# Example inputs
-# ----------------------------------------------------------------------------------------------------------------------
-
-
-genetic_code = get_genetic_code(string_type="DNA")
-input_text = "ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA"
-print(peptide_encoding(input_text, "MA"))
-# input_text = 'ATGGCC'
-# print(protein_translation(input_text, genetic_code))
+if __name__ == "__main__":
+    main()
